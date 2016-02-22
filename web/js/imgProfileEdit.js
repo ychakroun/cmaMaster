5b289017ace1a4138053f99775c8322d9c354117
Adrien Gaio <gaio.adrien@gmail.com>
2016-02-22 21:20:17 +0100

    pour un egal

--- web/js/imgProfileEdit.js
+++ web/js/imgProfileEdit.js
@@ -95,7 +95,7 @@ function setImageTemp(){
 	var image = document.getElementById('imgtmp').getAttribute('name');
 	var data = document.getElementById('imgtmp').innerHTML;
 	var id = image.slice(image.indexOf('[')+1,image.indexOf(']'));
-    if(id='imageHeader'){
+    if(id =='imageHeader'){
         document.getElementById(id).src = data;
     }else{
         id = id.replace(/[a-zA-Z]/g,'');
