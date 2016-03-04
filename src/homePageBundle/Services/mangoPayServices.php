ff289edd7b31e9172cd2bdf425427dc34bcc099c
Adrien Gaio <gaio.adrien@gmail.com>
2016-03-04 16:49:27 +0100

    mango pay initialization

---
+++ src/homePageBundle/Services/mangoPayServices.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace homePageBundle\Services;
+
+use MangoPay;
+
+
+class mangoPayServices
+{
+
+    private $mangoPayApi;
+
+    public function __construct()
+    {
+        $this->mangoPayApi = new MangoPay\MangoPayApi();
+        $this->mangoPayApi->Config->ClientId = 'custommyart';
+        $this->mangoPayApi->Config->ClientPassword = 'w1XjOt3b7rsNuOuqWYmi2EiyO8bE738VLjSEamkrOXht88MnnM';
+        $this->mangoPayApi->Config->TemporaryFolder = __DIR__.'/../../../var/tmpMangoPay';    
+        $this->mangoPayApi->Config->BaseUrl = 'https://api.sandbox.mangopay.com';
+    }
+
+    /**
+     * Create Mangopay User
+     * @return MangopPayUser $mangoUser
+     */
+    public function createMangoUser()
+    {
+
+        $mangoUser = new \MangoPay\UserNatural();
+        $mangoUser->PersonType = "NATURAL";
+        $mangoUser->FirstName = 'John';
+        $mangoUser->LastName = 'Doe';
+        $mangoUser->Birthday = 1409735187;
+        $mangoUser->Nationality = "FR";
+        $mangoUser->CountryOfResidence = "FR";
+        $mangoUser->Email = 'john.doe@mail.com';
+
+        //Send the request
+        $mangoUser = $this->mangoPayApi->Users->Create($mangoUser);
+
+        return $mangoUser;
+    }
+}
