ffbb81ec3d25aa9706429e606e2398ac852fab74
Vincent Miquel <vincent.miquel@fullsix.com>
2016-03-22 20:23:38 +0100

    create help page

--- src/homePageBundle/Controller/HelpController.php
+++ src/homePageBundle/Controller/HelpController.php
@@ -7,11 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManager;
 
-class ConceptController extends Controller
+class HelpController extends Controller
 {
 
     public function indexAction()
     {
-      	return $this->render('homePageBundle:Concept:index.html.twig');
+      	return $this->render('homePageBundle:Help:index.html.twig');
     }
 }
