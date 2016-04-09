a386bbe0df1631ee4299b7647bd880662304647c
Vincent Miquel <vincent.miquel@fullsix.com>
2016-04-09 12:29:45 +0200

    truncate description

--- web/js/jquery/adaptImage.js
+++ web/js/jquery/adaptImage.js
@@ -15,4 +15,28 @@ $(document).ready(function() {
      $(".js-description-hide").removeClass('hide');
    })
 
+
+   $(".js-description-more").each(function(){
+     var parent = $(this),
+     button = $(this),
+     chaine = $(this).text(),
+     LgtChaine = $.trim(chaine).length,
+     shortText = $.trim(chaine).substring(0, 300).split(" ").slice(0, -1).join(" ") + "...";
+
+     if (LgtChaine > 200){
+      parent.empty().html('<p>'+shortText+'</p>')
+
+       button.on('click', function(){
+         if(parent.hasClass('short')){
+           parent.removeClass('short');
+           parent.empty().html('<p>'+chaine+'</p>');
+         }else{
+           parent.addClass('short');
+           parent.empty().html('<p>'+shortText+'</p>');
+         }
+       })
+     }
+
+   });
+
 });
