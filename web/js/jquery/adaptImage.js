bd212d0ad687a6d80dea6a9ef581f797aacb2ce7
Vincent Miquel <vincent.miquel@fullsix.com>
2016-04-09 11:44:36 +0200

    modif after review Leo

--- web/js/jquery/adaptImage.js
+++ web/js/jquery/adaptImage.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-    $(".imgLiquidFill").imgLiquid();
-    $("#Glide").glide({
 
+    //Adapt image
+    $(".imgLiquidFill").imgLiquid();
+
+    //Slide
+    $("#Glide").glide({
       type: 'slider',
       centered: true,
       startAt: 1
-
    });
+
+   //Open description
+   $(".js-description-more").on('click',function(){
+     $(".js-description-hide").removeClass('hide');
+   })
+
 });
