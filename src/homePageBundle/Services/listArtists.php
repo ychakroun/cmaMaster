6b3ee1aadf9ee084ec2af1461d276ae8c97cfb7e
Adrien Gaio <gaio.adrien@gmail.com>
2016-04-22 00:11:27 +0200

    merge ;D

--- src/homePageBundle/Services/listArtists.php
+++ src/homePageBundle/Services/listArtists.php
@@ -13,20 +13,8 @@ class listArtists {
 
     public function indexAction() {
         $repository = $this->em->getRepository('CmaUserBundle:User');
-<<<<<<< HEAD
     	$listArtists = $repository->findByRoleAndOffset('ROLE_ARTIST',0);
         return ($listArtists);
-=======
-    	$listArtists = $repository->findByRoleIndex('ROLE_ARTIST');
-				dump(listArtists);
-        $artistformat = array();
-        foreach ($listArtists as $key => $artist) {
-            if($artist->getParameter()!=null&&$artist->getProfile()!=null&&$artist->getIsPublic()){
-                array_push($artistformat,$artist);
-            }
-        }
-        return ($artistformat);
->>>>>>> 621918915c39c5953f8a377a8fd538cb4755a403
     }
     public function getNbArtists() {
         $repository = $this->em->getRepository('CmaUserBundle:User');
@@ -52,29 +40,8 @@ class listArtists {
     public function pageArtist($offset) {
         $offset = $offset-1;
         $repository = $this->em->getRepository('CmaUserBundle:User');
-<<<<<<< HEAD
     	$listArtists = $repository->findByRoleAndOffset('ROLE_ARTIST',$offset);
         return ($listArtists);
-=======
-    	$listArtists = $repository->findByRole('ROLE_ARTIST');
-    	$i = 0;
-    	$y = 0;
-			$artistformat = array();
-    	foreach ($listArtists as $key => $artist) {
-            if($artist->getParameter()!=null&&$artist->getProfile()!=null&&$artist->getIsPublic()){
-    		  $tab = 'tab'.$key;
-    		  if($i == 6){
-    		  	$i = 0;
-    		  	$y++;
-    		  	$artistformat[$y][$i] = $artist;
-    		  }else{
-    		  	$artistformat[$y][$i] = $artist;
-    		  }
-    		  $i++;
-            }
-    	}
-        return ($artistformat);
->>>>>>> 621918915c39c5953f8a377a8fd538cb4755a403
     }
     public function getProfile($user) {
         $profile = $this->em
